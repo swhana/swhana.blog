f6215acd883a070206dc141a613d35b0b6f789b0
swhana <swhan0428@gmail.com>
2024-08-24 17:40:50 +0900

    Feat: Not found 페이지 추가 - Lottie 애니메이션 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import Lottie from "react-lottie-player";
+import LottieAnimation from "/public/assets/lotties/not-found";
+
 export default function NotFound() {
-    return <div>찾을 수 없는 페이지입니다.</div>;
+    return (
+        <div className="w-full flex justify-center items-center">
+            <Lottie
+                loop
+                animationData={LottieAnimation}
+                play
+                style={{
+                    width: "35vw",
+                }}
+            />
+        </div>
+    );
 }
