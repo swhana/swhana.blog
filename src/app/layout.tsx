790e1cee5a5249d68d2c11a3425b2f9cc38aa41d
swhana <swhan0428@gmail.com>
2024-08-24 16:58:02 +0900

    Feat: 헤더 추가, globals.css 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/config/globals.css";
+import Header from "@/layouts/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,13 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
+        <html lang="en" suppressHydrationWarning={true}>
+            <body className={inter.className}>
+                <Header />
+                <main className="mt-[64px] flex flex-1 flex-col">
+                    {children}
+                </main>
+            </body>
         </html>
     );
 }
---
+++ src/layouts/Header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navList = [
+    {
+        name: "Posts",
+        href: "/blog",
+    },
+    {
+        name: "About",
+        href: "/about",
+    },
+];
+
+export default function Header() {
+    const pathname = usePathname();
+    console.log(pathname?.startsWith("/blog"));
+
+    return (
+        <nav className="w-full z-40 flex flex-col justify-center items-center border-b bg-background shadow-sm print:hidden">
+            <div className="w-full max-w-[1200px] h-[64px] flex justify-between items-center mt-1 px-4">
+                <div className="flex items-center font-medium">
+                    {navList.map((nav) => (
+                        <Link
+                            href={nav.href}
+                            key={nav.name}
+                            className={cn(
+                                "rounded-full px-4 py-1 text-center text-sm transition-colors hover:text-primary",
+                                pathname?.startsWith(nav.href)
+                                    ? "font-bold text-primary"
+                                    : "text-muted-foreground",
+                            )}
+                        >
+                            {nav.name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </nav>
+    );
+}
