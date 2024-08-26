508e949cb6629c3e186b9eb83d4425b1f53c2ead
swhana <swhan0428@gmail.com>
2024-08-26 17:25:19 +0900

    Feat: Sidebar animation 추가

--- src/components/post_detail/ToC_Sidebar.tsx
+++ src/components/post_detail/ToC_Sidebar.tsx
@@ -17,9 +17,9 @@ export default function Sidebar({ contents }: TableProps) {
     if (contents.length === 0) return null;
 
     return (
-        <aside className="not-prose absolute -top-[200px] left-full -mb-[100px] hidden h-[calc(100%+150px)] xl:block">
+        <aside className="not-prose absolute -top-[200px] left-full -mb-[100px] hidden h-[calc(100%+150px)] xl:block ">
             <div className="sticky bottom-0  top-[200px] z-10 ml-[5rem] mt-[200px] w-[200px]">
-                <div className="mb-4 border-l px-4 py-2">
+                <div className="mb-4 border-l px-4 py-2 animate-slide-in-right-0.5">
                     {/* <div className="mb-1 font-bold">On this page</div> */}
                     <ul className="text-sm">
                         {contents.map((content) => {
@@ -46,7 +46,7 @@ export default function Sidebar({ contents }: TableProps) {
                         })}
                     </ul>
                 </div>
-                <div className="flex gap-2">
+                <div className="flex gap-2 animate-slide-in-right-1">
                     <ScrollToTop className="w-8 h-8" />
                 </div>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -186,10 +186,22 @@ const config = {
                     from: { height: "var(--radix-accordion-content-height)" },
                     to: { height: "0" },
                 },
+                "slide-in-right": {
+                    "0%": {
+                        transform: "translateX(20px)",
+                        opacity: "0",
+                    },
+                    "100%": {
+                        transform: "translateX(0)",
+                        opacity: "1",
+                    },
+                },
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
+                "slide-in-right-0.5": "slide-in-right 0.5s forwards",
+                "slide-in-right-1": "slide-in-right 1s forwards",
             },
         },
     },
