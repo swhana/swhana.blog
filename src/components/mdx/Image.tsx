013bb1589ea5e0943f5088231b7bfe4e270065b1
swhana <swhan0428@gmail.com>
2024-08-29 16:00:16 +0900

    Refactor: 이미지 태그 변경 - Nextjs/Image의 경우 원격 이미지를 사용할 때 이미지의 width / height property가 필요함 - 강제로 width height를 고정시켜서 사용하거나 부모 요소 크기에 맞춰서 사용한다 해도 이미지가 작은 경우 확대되서 화질이 깨지는 문제 발생 - 결국 최적화를 어느정도 포기하고 img 태그를 사용하기로 함 - 블로그 자체에 올리는 이미지를 처음부터 최적화 한 상태로 올리면 괜찮을듯함

--- src/components/mdx/Image.tsx
+++ src/components/mdx/Image.tsx
@@ -3,25 +3,19 @@ import Image from "next/image";
 interface ImageProps {
     src: string;
     alt: string;
-    width?: number;
-    height?: number;
 }
 
-export const MdxImage = ({
-    src,
-    alt,
-    width = 500,
-    height = 500,
-}: ImageProps) => {
+export const MdxImage = ({ src, alt }: ImageProps) => {
+    //블로그에 올라가는 이미지는 전부 원격 이미지가 되기 때문에 사이즈를 사전에 알 수가 없음 그래서 img태그 활용
     return (
         <div>
-            <Image
-                src={src}
-                alt={alt}
-                className="mx-auto mb-0 mt-8 rounded-md"
-                width={width}
-                height={height}
-            />
+            <picture>
+                <img
+                    src={src}
+                    alt={alt}
+                    className="mx-auto mb-0 mt-8 rounded-md"
+                />
+            </picture>
             {alt !== "" && (
                 <span className="w-full block mb-8 mt-2 text-center text-sm text-gray-500 dark:text-gray-400">
                     {alt}
