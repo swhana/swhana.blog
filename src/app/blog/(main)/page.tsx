ad340f955375fe9692b9d7a488830bca9d855d57
swhana <swhan0428@gmail.com>
2024-08-30 15:53:22 +0900

    Fix: 빌드 오류 수정 완료 - node.js 모듈의 fs, path 등의 함수는 서버 사이드에서 실행되어야 한다 - 그러므로 이를 포함한 posts.ts 유틸모듈은 전부 서버 사이드에서만 실행되어야 함 - app 디렉토리 이외의 디렉토리에서 이 함수들이 호출되면 클라이언트 사이드에서 실행되는 것으로 간주해 빌드 오류가 발생한다 - 모든 데이터를 불러오는 로직은 app 디렉토리 하에서 실행해야 오류가 발생하지 않는다

--- src/app/blog/(main)/page.tsx
+++ src/app/blog/(main)/page.tsx
@@ -1,7 +1,34 @@
 //카테고리를 아예 입력 안했을 때
 
-import PostListPage from "@/pages/post_list/AllPostListPage";
+import CategoryList from "@/components/post_list/CategoryList";
+import PostCard from "@/components/post_list/PostCard";
+import {
+    getAllPostCount,
+    getCategoryDetailList,
+    getSortedPostList,
+} from "@/lib/posts";
 
-export default async function Blog() {
-    return <PostListPage />;
-}
+const PostListPage = async () => {
+    const postList = await getSortedPostList();
+    const categoryList = await getCategoryDetailList();
+    const allPostCount = await getAllPostCount();
+
+    return (
+        <section className="mx-auto mt-[72px] w-full max-w-[750px] px-4">
+            <CategoryList
+                categoryList={categoryList}
+                allPostCount={allPostCount}
+                currentCategory={"All"}
+            />
+            <section>
+                <ul className="flex flex-col gap-8 lg:gap-12">
+                    {postList.map((post) => (
+                        <PostCard key={post.url + post.date} post={post} />
+                    ))}
+                </ul>
+            </section>
+        </section>
+    );
+};
+
+export default PostListPage;
