0c2b01e5e49936487c735538614b8ef38ec7efb3
swhana <swhan0428@gmail.com>
2024-08-31 14:03:42 +0900

    Post: 빌드하면서 겪은 오류

---
+++ src/components/post_list/PostListPage2.tsx
@@ -0,0 +1,36 @@
+// 전체 포스트 썸네일 보여주는 개요 페이지
+
+import CategoryList from "@/components/post_list/CategoryList";
+import PostCard from "@/components/post_list/PostCard";
+import { getAllPostCount, getCategoryDetailList, getSortedPostList } from "@/lib/posts";
+
+interface PostListProps {
+    category?: string;
+}
+
+const PostListPage = async ({
+    category,
+
+}: PostListProps) => {
+    const postList = await getSortedPostList(category);
+    const categoryList = getCategoryDetailList();
+    const allPostCount = await getAllPostCount();
+    return (
+        <section className="mx-auto mt-[72px] w-full max-w-[750px] px-4">
+            <CategoryList
+                categoryList={categoryList}
+                allPostCount={allPostCount}
+                currentCategory={category}
+            />
+            <section>
+                <ul className="flex flex-col gap-8 lg:gap-12">
+                    {postList.map((post) => (
+                        <PostCard key={post.url + post.date} post={post} />
+                    ))}
+                </ul>
+            </section>
+        </section>
+    );
+};
+
+export default PostListPage;
