0712e073c087d3b08b308ca448a25c5deef752b9
swhana <swhan0428@gmail.com>
2024-09-11 22:34:48 +0900

    Feat: Add Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,11 @@
 import AboutPage from "@/components/about/AboutPage";
 import { getSortedProjectList } from "@/lib/projects";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: { absolute: "swhana | FE Engineer" },
+    description: "프론트엔드 개발자 한상원입니다.",
+};
 
 const AboutMe = async () => {
     const projects = await getSortedProjectList();
